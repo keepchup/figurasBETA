e2f2e860b9e1ec69ad32ddde7bf55865f8ee0c35
Luis Adrian Ramos <150483323+keepchup@users.noreply.github.com>
2024-06-01 18:18:22 -0500

    ClaseCuadrilateros

--- src/FIGURAS/Cuadrilateros.java
+++ src/FIGURAS/Cuadrilateros.java
@@ -4,4 +4,12 @@ public class Cuadrilateros {
 
     public Cuadrilateros() {
     }
+    @Override
+    public int getBase() {
+        return super.getBase();
+    }
+
+    public double calcularAreaCuadrado() {
+        return
+    }
 }
