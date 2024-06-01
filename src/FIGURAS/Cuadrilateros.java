0d80e0f3a1d99f0cb1fe425492bddc48e83537aa
Luis Adrian Ramos <150483323+keepchup@users.noreply.github.com>
2024-06-01 18:45:36 -0500

    ClaseCuadrilateros3

--- src/FIGURAS/Cuadrilateros.java
+++ src/FIGURAS/Cuadrilateros.java
@@ -15,18 +15,26 @@ public class Cuadrilateros extends Dos_D{
         return super.getArea();
     }
 
-    public double calcularAreaCuadrado() {
+    public double calcularDatosCuadrado() {
         area = base*base;
+        perimetro = base*4;
+
         return area;
     }
-
-    public double calcularPerimetroCuadrado() {
-        perimetro = base*4;
-        return perimetro;
-    }
-
     public void imprimirdATOSCuadrado() {
         System.out.println("EL area del cuadrado es: " + getArea());
         System.out.println("El perimetro del cuadrado es: " + getPerimetro());
+    }
+
+    public double calcularDatosRectangulo(){
+        area = 0;
+        perimetro = 0;
+        area = base*altura;
+        perimetro = (base*2)+(altura*2);
+        return area;
+    }
+    public void imprimirDatosRectangulo() {
+        System.out.println("EL area del rectangulo es: " + getArea());
+        System.out.println("El perimetro del rectangulo es: " + getPerimetro());
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,21 @@ public class Main {
 
         Cuadrilateros c1 = new Cuadrilateros();
 
+        Cuadrilateros c2 = new Cuadrilateros();
+
         c1.setLados(4);
         c1.setBase(3);
-        c1.calcularAreaCuadrado();
+        c1.calcularDatosCuadrado();
         c1.imprimirdATOSCuadrado();
+
+        c2.setLados(4);
+        c2.setBase(3);
+        c2.setAltura(5);
+        c2.calcularDatosRectangulo();
+        c2.imprimirDatosRectangulo();
+
+
+
     }
+
 }
