3d86176dda068c73abdfaaffe042e211ee235d82
DESKTOP-JKP0THG\Usuario <carlos.perez02@epn.edu.ec>
2024-06-01 17:34:10 -0500

    Clase dos

---
+++ src/FIGURAS/Dos_D.java
@@ -0,0 +1,35 @@
+package FIGURAS;
+
+public class Dos_D extends Figuras {
+
+    double area;
+    double perimetro;
+
+    public Dos_D() {
+    }
+
+    public Dos_D(double area, double perimetro) {
+        this.area = area;
+        this.perimetro = perimetro;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public double getPerimetro() {
+        return perimetro;
+    }
+
+    public void setPerimetro(double perimetro) {
+        this.perimetro = perimetro;
+    }
+
+    public double calcularArea() {}
+
+    public double calcularPerimetro() {}
+}
