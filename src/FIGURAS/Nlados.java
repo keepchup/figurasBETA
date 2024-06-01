a12f54dae1942da390075d5dad31f77ff5ab35ac
Luis Adrian Ramos <150483323+keepchup@users.noreply.github.com>
2024-06-01 17:54:40 -0500

    ClaseNlados

---
+++ src/FIGURAS/Nlados.java
@@ -0,0 +1,22 @@
+package FIGURAS;
+
+public class Nlados extends Otras_Figuras{
+    double apotema;
+
+    public Nlados() {
+    }
+
+    public Nlados(double apotema) {
+        this.apotema = apotema;
+    }
+
+    public double getApotema() {
+        return apotema;
+    }
+
+    public void setApotema(double apotema) {
+        this.apotema = apotema;
+    }
+
+
+}
